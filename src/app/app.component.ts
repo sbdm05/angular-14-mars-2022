342100b657486b6333eb8dc2881570633bb3461e
Sonia-Baibou <s.baibou@ohmycode.io>
2022-03-14 14:15:25 +0100

    étape 1 - modules

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+// décorateur
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  public title = 'angular-14-mars-2022';
+
+
+}// ne rien écrire en dehors de cette classe
