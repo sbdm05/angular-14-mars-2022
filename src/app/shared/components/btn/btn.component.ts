8f3da5d157891c65766763b4b9cff029d76cad6e
Sonia-Baibou <s.baibou@ohmycode.io>
2022-03-17 14:32:20 +0100

    bouton vers formulaire

--- src/app/shared/components/btn/btn.component.ts
+++ src/app/shared/components/btn/btn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-btn',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./btn.component.scss']
 })
 export class BtnComponent implements OnInit {
+
+  @Input() color!: string;
+  @Input() route!: string;
+  @Input() label!: string;
 
   constructor() { }
 
